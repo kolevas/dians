5dbc23c25681eddf230ea171d1096555e3e6172b
kolevas <138041443+kolevas@users.noreply.github.com>
2024-12-05 00:01:33 +0100

    Delete hw2/mseJava/makedonskaBerza2 directory

--- hw2/mseJava/makedonskaBerza2/src/main/java/mk/finki/ukim/mk/makedonskaberza/repository/IssuerRepository.java
+++
@@ -1,7 +0,0 @@
-package mk.finki.ukim.mk.makedonskaberza.repository;
-
-import mk.finki.ukim.mk.makedonskaberza.model.Issuer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssuerRepository extends JpaRepository<Issuer, String> {
-}
--- hw2/mseJava/makedonskaBerza2/src/main/java/mk/finki/ukim/mk/makedonskaberza/service/IssuerService.java
+++
@@ -1,9 +0,0 @@
-package mk.finki.ukim.mk.makedonskaberza.service;
-
-import mk.finki.ukim.mk.makedonskaberza.model.Issuer;
-
-import java.util.List;
-
-public interface IssuerService {
-    public List<Issuer> GetAllIssuers();
-}
